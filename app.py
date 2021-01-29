d47a136113308573ddb9ceeeac1a68693ea26541
Mateus <mateussimashadlich@gmail.com>
2021-01-28 23:10:27 -0300

    Métodos GET

--- app.py
+++ app.py
@@ -20,7 +20,13 @@ def create_app():
 
 app = create_app()
 
-@app.route('/prova', methods=['POST'])
+
+@app.route('/prova', methods=['POST', 'GET'])
+def prova():
+    if request.method == 'POST':
+        return cadastrar_prova()
+    elif request.method == 'GET':
+        return visualizar_provas()
 def cadastrar_prova():
     try:
         if request.get_json() == None:
@@ -46,8 +52,20 @@ def cadastrar_prova():
         db.session.rollback()
         return cadastrar_prova()
 
+def visualizar_provas():
+    provas = []
+    for prova in Prova.query.all():
+        provas.append({'id': prova.id, 'matricula_aluno': prova.matricula_aluno})
 
-@app.route('/gabarito', methods=['POST'])
+    return jsonify(provas=provas)
+
+@app.route('/gabarito', methods=['POST', 'GET'])
+def gabarito():
+    if request.method == 'POST':
+        return cadastrar_gabarito()
+    elif request.method == 'GET':
+        return visualizar_gabaritos()
+
 def cadastrar_gabarito():
     try:
         if request.get_json() == None:
@@ -87,8 +105,31 @@ def cadastrar_gabarito():
                         documentacao="https://github.com/mateussimashadlich/escola_alf/blob/master/README.md"
                     ), 400         
     
+def visualizar_gabaritos():
+    gabaritos = []
+    for prova_gabarito in db.session.query(Gabarito.id_prova.distinct()).all():
+        questoes_gabarito = []
+        for questao_gabarito in Gabarito.query.filter_by(id_prova=prova_gabarito[0]).all(): 
+            questoes_gabarito.append({
+                'num_questao': questao_gabarito.num_questao, 
+                'peso_questao': questao_gabarito.peso_questao,
+                'alternativa': questao_gabarito.alternativa
+            })
 
-@app.route('/resposta', methods=['POST'])
+        gabaritos.append({
+        'id_prova': prova_gabarito[0], 
+        'questoes': questoes_gabarito
+        })
+
+    return jsonify(gabaritos=gabaritos)
+
+@app.route('/resposta', methods=['POST', 'GET'])
+def resposta():
+    if request.method == 'POST':
+        return cadastrar_resposta()
+    elif request.method == 'GET':
+        return visualizar_respostas()
+
 def cadastrar_resposta():
     try:
         if request.get_json() == None:
@@ -129,6 +170,23 @@ def cadastrar_resposta():
                         respostas_cadastradas=respostas_cadastradas
                     )                                  
     
+def visualizar_respostas():
+    respostas = []
+    for matricula_aluno in db.session.query(Resposta.matricula_aluno.distinct()).all():
+        respostas_aluno = []
+        for resposta in Resposta.query.filter_by(matricula_aluno=matricula_aluno[0]).all(): 
+            respostas_aluno.append({
+                'num_questao': resposta.num_questao,                 
+                'alternativa': resposta.alternativa
+            })
+
+        respostas.append({
+        'matricula_aluno': matricula_aluno[0], 
+        'respostas_aluno': respostas_aluno
+        })
+
+    return jsonify(respostas=respostas)
+
 
 @app.route('/nota_final', methods=['GET'])
 def get_nota_final_aluno():
@@ -165,10 +223,11 @@ def verificar_nota(matricula_aluno):
 
 @app.route('/alunos_aprovados', methods=['GET'])
 def get_alunos_aprovados():
-    alunos = db.session.query(Prova.matricula_aluno.distinct()).all()
-    for aluno in alunos:
-        if verificar_nota(aluno[0]).get_json()['nota_final'] < 7:
-            alunos.remove(aluno)
+    alunos = []
+    for aluno in db.session.query(Prova.matricula_aluno.distinct()).all():
+        #aluno[0] equivale à matricula do aluno
+        if verificar_nota(aluno[0]).get_json()['nota_final'] >= 7:     
+            alunos.append(aluno[0])
 
     return jsonify(matricula_alunos_aprovados=alunos)
 
